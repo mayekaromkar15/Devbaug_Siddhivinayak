6fa869d2acb318a39c74854a2ec032faabf5593a
Omkar Mayekar <mayekaromkar15@gmail.com>
2022-02-13 19:54:48 +0530

    Uploaded complete website

---
+++ script.js
@@ -0,0 +1,28 @@
+var preloader = document.getElementById("pre-loader");
+const navBtn = document.getElementById("nav-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const sideNav = document.getElementById("sidenav");
+const modal = document.getElementById("modal");
+
+navBtn.addEventListener("click", function () {
+  sideNav.classList.add("show");
+  modal.classList.add("showModal");
+});
+
+cancelBtn.addEventListener("click", function () {
+  sideNav.classList.remove("show");
+  modal.classList.remove("showModal");
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    sideNav.classList.remove("show");
+    modal.classList.remove("showModal");
+  }
+});
+
+function myloader() {
+  setTimeout(function () {
+    preloader.style.display = "none";
+  }, 5000);
+}
